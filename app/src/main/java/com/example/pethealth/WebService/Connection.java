6eb34621d5461d370ad1a5eb77e8a989546473f9
Kaique <kaiqueadm@hotmail.com>
2019-06-19 12:25:03 -0300

    atualização final do projeto 1.4

--- app/src/main/java/com/example/pethealth/WebService/Connection.java
+++ app/src/main/java/com/example/pethealth/WebService/Connection.java
@@ -3,7 +3,7 @@ package com.example.pethealth.WebService;
 public class Connection {
 
 
-    private static String url = "http://10.12.5.163:8080/PetHealth/rest/app/";
+    private static String url = "http://192.168.1.21:8080/PetHealth/rest/app/";
 
 
     //192.168.1.25 notebook;
